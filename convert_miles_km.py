f037cb773c21f2e290c57405d1a9e07bbf597718
Nurul Syafini <nurulsyafinisubhan.bintemohamedaidilsubhan@my.jcu.edu.au>
2022-05-05 19:09:00 +0800

    Add docstrings to functions

--- convert_miles_km.py
+++ convert_miles_km.py
@@ -6,18 +6,22 @@ CONVERT_MILES_TO_KM = 1.60934
 
 
 class ConvertMilesToKmApp(App):
+    """ ConvertMilesToKmApp is a Kivy App to convert Miles to Kilometres"""
     output_km = StringProperty()
 
     def build(self):
+        """Build the Kivy app from the kv file"""
         self.title = "Convert Miles to Km"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def handle_calculate(self, text):
+        """Handle calculation (could be button press or other call)"""
         miles = self.convert_number(text)
         self.update_result(miles)
 
     def handle_increment(self, text, change):
+        """Handle up or down button press, text input updates with new value, calculation function"""
         miles = self.convert_number(text) + change
         self.root.ids.input_number.text = str(miles)
 
@@ -26,6 +30,7 @@ class ConvertMilesToKmApp(App):
 
     @staticmethod
     def convert_number(text):
+        """Convert text to float, if invalid input show 0.0"""
         try:
             return float(text)
         except ValueError:
