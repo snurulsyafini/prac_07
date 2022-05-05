0ffba908199951c5faf2445e03c302001fa3b159
Nurul Syafini <nurulsyafinisubhan.bintemohamedaidilsubhan@my.jcu.edu.au>
2022-05-05 16:31:09 +0800

    Add new button functions

--- box_layout_demo.py
+++ box_layout_demo.py
@@ -8,5 +8,14 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        print("Greet")
+        your_name = self.root.ids.input_name.text
+        self.root.ids.output_label.text = "Hello " + your_name
 
-BoxLayoutDemo().run()
+    def handle_clear(self):
+        self.root.ids.input_name.text = ""
+        self.root.ids.output_label.text = ""
+
+if __name__ == '__main__':
+    BoxLayoutDemo().run()
